293ea3eebb78a398eb9f2437e9c1a622c55a4790
Antonio Robles <antjrobles@gmail.com>
2023-01-12 10:23:29 +0100

    MongoDB connection and users creation ok. Video nª 1 done

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,27 +1,27 @@
-const userModels = require('../Models/userModel'); // Import userModels.js
+const userModel = require('../Models/userModel'); // Import userModels.js
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
 
 const createToken = (_id) => {
-    const jwtkey = process.env.JWT_SECRETE_KEY;
+    const jwtkey = process.env.JWT_SECRET_KEY;
 
-    return jwt.sign({ _id }, jwtkey, { expiresIn: '3d' }) // 3 day token expire
+    return jwt.sign({ _id }, jwtkey, { expiresIn: '3d' }); // 3 day token expire
 };
 
 const registerUser = async (req, res) => {
     try {
-    const { name, email, password } = req.body // Get data from body
+    const { name, email, password } = req.body; // Get data from body
 
     let user = await userModel.findOne({ email }); // Check if email already exists
      
-    if (user) return res.status(400).json({ message: 'Email already exists' }); // If email exists, return error message.
+    if (user) return res.status(400).json('Email already exists'); // If email exists, return error message.
 
-    if (!name || !email || !password) return res.status(400 ).json({ message: 'Please enter all fields' }); // If any field is empty, erro msg
+    if (!name || !email || !password) return res.status(400 ).json('Please enter all fields'); // If any field is empty, erro msg
 
-    if (!validator.isEmail(email)) return res.status(400).json({ message: 'Please enter a valid email' }); // If email is not valid, error msg
+    if (!validator.isEmail(email)) return res.status(400).json('Please enter a valid email'); // If email is not valid, error msg
 
-    if (!validator.isStrongPassword(password)) return res.status(400).json({ message: 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character' }); // If password is not valid, error msg.
+    if (!validator.isStrongPassword(password)) return res.status(400).json('Password must be a strong password...'); // If password is not valid, error msg.
 
 
     user = new userModel({ name, email, password }); // Create new user object.
@@ -33,13 +33,57 @@ const registerUser = async (req, res) => {
 
     const token = createToken(user._id); // Create token.
 
-    rest.status(200).json({_id: user._id, name, email, token}); // Return token.
-    } catch(error){
-        console.log(error)
+    res.status(200).json({_id: user._id, name, email, token}); // Return token.
+    } catch (error) {
+        console.log(error);
         res.status(500).json(error); // If error, return error.
     }
     };
 
 
-module.exports = { registerUser }; // Export registerUser function to be used in other files. 
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body; // Get data from body.
+
+    try {
+        let user = await userModel.findOne({ email }); // Check if email exists.
+
+        if (!user) return res.status(400).json("Invalid Email or Password"); // If email does not exist, return error.
+            const isValidPassword = await bcrypt.compare(password, user.password); // Check if password matches.
+
+            if (!isValidPassword) return res.status(400).json("Invalid Email or Password"); // If password does not match, error msg.
+
+            const token = createToken(user._id); // Create token.
+        res.status(200).json({ _id: user._id, name: user.name, email, token }); // Return token.
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error); // If error, return error.
+};
+};
+
+
+const findUser = async (req, res) => {
+    const userId = req.params.userId;      // Get user id from params.
+    try {
+        const user = await userModel.findById(userId) // Find user by id.
+        res.status(200).json(user); // Return user.
+
+    } catch (error) { // If error, return error.
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await userModel.find() // Find all users.
+        res.status(200).json(users); // Return user.
+
+    } catch (error) { // If error, return error.
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+module.exports = { registerUser, loginUser, findUser, getUsers }; // Export functions.
--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,11 +1,15 @@
 const express = require('express'); 
-const {registerUser} = require("../Controllers/userController"); //importing the controller function from userController.js
+const { registerUser, loginUser, findUser, getUsers } = require("../Controllers/userController"); //importing the controller function from userController.j
 
 const router = express.Router(); 
 
 
 
 router.post('/register', registerUser); //registerUser is the controller function imported from userController.js 
+router.post('/login', loginUser); //loginUser is the controller function imported from userController.js
+router.get('/find/:userId', findUser); //findUser is the controller function imported from userController.js 
+router.get('/', getUsers); //getUsers is the controller function imported from userController.js
+
 
 module.exports = router; // export router to use in index.js
 
