1bf1ae69d2c072cd5ae9e7aa1baf9e1e6405cce6
Antonio Robles <antjrobles@gmail.com>
2023-01-16 10:44:48 +0100

    Video nº 6 ok

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Logo from "../assets/AR2.svg";
-
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 
 
 const NavBar = () => {
+
+  const { user } = useContext(AuthContext); // Get user from AuthContext
+
   return <Navbar bg="info" className="mb-4" style={{ height: "5.75rem" }}>
     <Container>
       <h2>
@@ -13,7 +17,7 @@ const NavBar = () => {
           <img src={Logo} alt="ChatApp"  width="65" height="65" />
         </Link>
       </h2>
-      <span className="text-success">Logged in as Antonio</span>
+      <span className="text-success">Logged in as {user?.name}</span> //
       <Nav> 
         <Stack direction="horizontal" gap={5}>
         <Link to="/login" className="link-dark text-decoration-none">
