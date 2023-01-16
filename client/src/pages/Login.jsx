25496d8b128d688e661d67206637d5321eeeadd4
Antonio Robles <antjrobles@gmail.com>
2023-01-16 13:41:10 +0100

    Video nº 6 ok

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
   const { loginInfo, updateLoginInfo, loginUser, loginError, isLoginLoading } =
-    useContext(AuthContext);
+    useContext(AuthContext); 
 
   return (
     <>
